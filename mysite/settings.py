de39eade8891b0d172a46ab013c4ca59dabdc973
askvart <tur1amant@gmail.com>
2021-12-28 16:17:03 +0300

    media

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- polls/models.py
+++ polls/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Vote(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateField(auto_now_add=True)
+    poster = models.ImageField(upload_to='posters/', blank=True)
 
     def __str__(self):
         return self.title
